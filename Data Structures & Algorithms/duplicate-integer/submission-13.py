14595afb8dbf59b52935ff29265f56929ee23081
yourisev <69630866+yourisev@users.noreply.github.com>
2026-09-22 11:51:45 -0700

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.py
@@ -0,0 +1,14 @@
+# O(n) Time Complexity and O(n) Space Complexity
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        
+        return False
+        
